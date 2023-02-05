97f80ba60876f3885c9709490c655a21eae0f157
Aerdellosi <johnhanifin.webdev@gmail.com>
2023-02-05 12:32:04 -0800

    first passed test

--- __tests__/business.test.js
+++ __tests__/business.test.js
@@ -1,2 +1,10 @@
 import { UserAgeInterval } from "../src/business";
 
+describe("UserAgeInterval", () => {
+    test("should create a user object with age interval", () => {
+        const newUser1 = new UserAgeInterval(12, 34);
+        expect(newUser1.initialAge).toEqual(12);
+        expect(newUser1.endingAge).toEqual(34);
+    });
+
+});
