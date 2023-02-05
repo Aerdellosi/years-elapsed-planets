ba7762bcce9f7977ddc55fa59ae13b1ac784a9f9
Aerdellosi <johnhanifin.webdev@gmail.com>
2023-02-05 13:36:46 -0800

    finish tests for jupiter and planet X

--- __tests__/business.test.js
+++ __tests__/business.test.js
@@ -19,7 +19,6 @@ describe("yearsElapsedPlanets", () => {
         const newUser1 = new UserAgeInterval(12, 34);
         const intervalDifference = newUser1.findIntervalDifference();
         const marsYearsTotal = yearsElapsedPlanets.yearsElapsedMars(intervalDifference);
-        console.log(marsYearsTotal);
         expect(marsYearsTotal).toEqual(11.69);
     });
     test("should calculate years elapsed on earth given earth years interval", () => {
@@ -27,7 +26,19 @@ describe("yearsElapsedPlanets", () => {
         const intervalDifference = newUser1.findIntervalDifference();
         const earthYearsTotal = yearsElapsedPlanets.yearsElapsedEarth(intervalDifference);
         expect(earthYearsTotal).toEqual(22);
-
-    })
+    });
+    test("should calculate years elapsed on jupiter given earth years interval", () => {
+        const newUser1 = new UserAgeInterval(12, 34);
+        const intervalDifference = newUser1.findIntervalDifference();
+        const jupiterYearsTotal = yearsElapsedPlanets.yearsElapsedJupiter(intervalDifference);
+        expect(jupiterYearsTotal).toEqual(1.85);
+    });
+    test("should calculate years elapsed on planet X given earth years interval", () => {
+        const newUser1 = new UserAgeInterval(12, 34);
+        const intervalDifference = newUser1.findIntervalDifference();
+        const planetXYearsTotal = yearsElapsedPlanets.yearsElapsedPlanetX(intervalDifference);
+        console.log(planetXYearsTotal);
+        expect(planetXYearsTotal).toEqual(0.80);
+    });
 
 });
